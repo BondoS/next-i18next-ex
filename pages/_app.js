3f042843ea0e2f15a9b525c343972913b5f821f7
Abdelrahman Serag <serag@serags-MacBook-Pro.local>
2019-07-30 12:11:56 +0200

    pass other langs to all child components

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,29 @@
-
-import React from 'react'
-import App, { Container } from 'next/app'
-import { appWithTranslation } from '../i18n'
+import React from "react";
+import App, { Container } from "next/app";
+import { appWithTranslation } from "../i18n";
+import { lngFromReq } from "next-i18next/dist/commonjs/utils";
+import { i18n } from "../i18n";
 
 class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps, otherLangs, lng;
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+      lng = await lngFromReq(ctx.req);
+      otherLangs = await i18n.options.allLanguages.filter(item => item != lng);
+    }
+
+    return { pageProps, otherLangs };
+  }
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps, otherLangs } = this.props;
     return (
       <Container>
-        <Component {...pageProps} />
+        <Component {...pageProps} otherLangs={otherLangs} />
       </Container>
-    )
+    );
   }
 }
 
-export default appWithTranslation(MyApp)
+export default appWithTranslation(MyApp);
